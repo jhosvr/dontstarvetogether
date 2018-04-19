593f0020b5cc84d2ee35276ca8c0b27ccaacccf8
Justin Ho <hojc08@gmail.com>
2018-04-19 04:02:40 +0000

    initial script and defaults

--- scripts/setupServer.sh
+++ scripts/setupServer.sh
@@ -17,13 +17,22 @@ usage() {
 # Error checking functions. Parameter format is $1=object $2=message-to-return $3=action-to-take
 fileNotExists() { if [[ ! -f "${1}" ]]; then echo "${2}" && ${3}; fi; }
 dirNotExists() { if [[ ! -d "${1}" ]]; then echo "${2}" && ${3}; fi; }
+dirExists() { if [[ -d "${1}" ]]; then echo "${2}" && ${3}; fi; }
+isEmpty() { if [[ -z "${1}" ]]; then echo "${2}" && ${3}; fi; }
+checkFail() { if [[ "$?" != "0" ]]; then echo "ERROR: failed at step ${step}" && exit 2; fi; }
 
+####
 #### DEFAULT VARIABLES
+####
 
 conf="../conf/default.conf"
 dstBase="$HOME/.klei/DoNotStarveTogether"
 
+
+####
 #### OVERRIDES
+####
+step="[establish-overrides]"
 
 while [[ ! -z "${1}" ]]; do
   case "${1}" in
@@ -38,7 +47,12 @@ while [[ ! -z "${1}" ]]; do
   esac
 done
 
+checkFail
+
+####
 #### EXECUTION CHECKS
+####
+step="[execution-checks]"
 
 fileNotExists "${conf}" \
   "ERROR: unable to locate conf file ${conf}" \
@@ -47,22 +61,45 @@ dirNotExists "${dstBase}" \
   "ERROR: The folder ${dstBase} does not exist. Please install the game or specify another location with the -base parameter." \
   "exit 1"
 
+tokenCheck=$(grep "cluster_token=" "${conf}" | awk -F'=' '{print $2}')
+isEmpty "${tokenCheck}" \
+  "ERROR: cluster_token variable was not set. Please check ${conf}, the token is required to run a server." \
+  "exit 1"
+serverName=$(grep "cluster_name=" "${conf}" | awk -F'=' '{print $2}')
+dirExists "${dstBase}/${serverName}" \
+  "ERROR: ${dstBase}/${serverName} already exists. This script will exit so it does not harm any existing files." \
+  "exit 1"
+if [[ "${conf}" == "../conf/default.conf" ]]; then
+  echo "WARNING: a seperate conf file was not specified. Applying defaults."
+  fi
 
+checkFail
+
+####
 #### SCRIPT EXECUTION
-setupBase="${dstBase}"/"${cluster_name}"
+####
+step="[script-execution]"
+setupBase="${dstBase}"/"${serverName}"
 
-# Create dedicated server folders
-source <(sed -n '/# SECTION: server\/cluster.ini/,/# SECTION: .*/p' "${conf}")
+
+step="[create-server-folders]"
+source <(sed -n '/#### SECTION: server\/cluster.ini/,/#### SECTION: .*/p' "${conf}")
+echo "${step} Creating server instance folders"
 mkdir -p "${setupBase}"/Master
 mkdir -p "${setupBase}"/Caves
+checkFail
 
-# Create cluster token file
-source <(sed -n '/# SECTION: cluster_token.txt/,/# SECTION: .*/p' "${conf}")
+
+step="[create-cluster-token]"
+source <(sed -n '/#### SECTION: cluster_token.txt/,/#### SECTION: .*/p' "${conf}")
+echo "${step} Creating cluster token file"
 echo "${cluster_token}" > "${setupBase}/cluster_token.txt"
+checkFail
 
-# Create base cluster.ini file
-echo "
-[GAMEPLAY]
+
+step="[create-cluster.ini]"
+echo "${step} Generating ${cluster_name}/cluster.ini"
+echo "[GAMEPLAY]
 game_mode = $game_mode
 max_players = $max_players
 pvp = $pvp
@@ -71,7 +108,7 @@ vote_enabled = $vote_enabled
 
 [NETWORK]
 offline_cluster = $offline_cluster
-cluster_description = $cluster_description
+cluster_description = \"$cluster_description\"
 cluster_name = \"$cluster_name\"
 cluster_intention = $cluster_intention
 cluster_password = $cluster_password
@@ -91,14 +128,51 @@ master_ip = $master_ip
 master_port = $master_port
 cluster_key = $cluster_key
 " > "${setupBase}"/cluster.ini
+checkFail
 
 
-# Create /server/Master/server.ini
+step="[create-master-server.ini]"
+echo "${step} Generating Master/server.ini"
+source <(sed -n '/#### SECTION: server\/Master\/server.ini/,/#### SECTION: .*/p' "${conf}")
+echo "[NETWORK]
+server_port = $server_port
+
+[SHARD]
+is_master = $is_master
+name = $name
+id = $id
+
+[STEAM]
+master_server_port = $master_server_port
+authentication_port = $authentication_port
+" > "${setupBase}"/Master/server.ini
+checkFail
 
 
+step="[create-caves-server.ini]"
+echo "${step} Generating Caves/server.ini"
+source <(sed -n '/#### SECTION: server\/Caves\/server.ini/,/#### SECTION: .*/p' "${conf}")
+echo "[NETWORK]
+server_port = $server_port
+
+[SHARD]
+is_master = $is_master
+name = $name
+id = $id
+
+[STEAM]
+master_server_port = $master_server_port
+authentication_port = $authentication_port
+" > "${setupBase}"/Caves/server.ini
+checkFail
 
 
-
-
-
+step="[create-caves-worldgen]"
+echo "${step} Generating Caves/worldgenoverride.lua"
+echo "return {
+  override_enabled = true,
+  preset = "DST_CAVE",
+}
+" > "${setupBase}"/Caves/worldgenoverride.lua
+checkFail
 
